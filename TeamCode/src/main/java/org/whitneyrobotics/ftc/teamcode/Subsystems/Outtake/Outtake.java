d156a3e7028b5530b7ec85d1b1e4c4015baa9385
Anik750 <92497487+Anik750@users.noreply.github.com>
2024-12-21 03:14:31 +0000

    Update Outtake.java

--- TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/Subsystems/Outtake/Outtake.java
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/Subsystems/Outtake/Outtake.java
@@ -1,7 +1,7 @@
 // Written By: Anik Koirala
 
 // package:
-package org.firstinspires.ftc.teamcode.Subsystems.Outtake;
+package org.whitneyrobotics.ftc.teamcode.Subsystems.Outtake;
 
 public class Outtake {
 }
