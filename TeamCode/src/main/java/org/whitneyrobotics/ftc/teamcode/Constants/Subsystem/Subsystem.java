2c78a57cd5712ffa948c8f4d07d4f6ab6d3a3b36
Anik750 <92497487+Anik750@users.noreply.github.com>
2024-12-21 02:49:31 +0000

    Update Subsystem.java

---
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/Constants/Subsystem/Subsystem.java
@@ -0,0 +1,13 @@
+// Written by: Christopher Gholmieh (reviewed by Anik Koirala)
+// Package:
+package org.whitneyrobotics.ftc.teamcode.Constants.Subsystem;
+
+// Imports:
+import org.whitneyrobotics.ftc.teamcode.Constants.Channel.Channel;
+
+
+// Interface:
+public interface Subsystem {
+    // Methods:
+    public void update(Channel channel);
+}
